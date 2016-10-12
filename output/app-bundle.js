c48832b7bf76aa8f0abbf9d87d32104980699caa
Meirion Hughes <mhughes@geologging.com>
2016-10-12 17:04:32 +0100

    stash

---
+++ output/app-bundle.js
@@ -0,0 +1,47 @@
+define('app',["require", "exports"], function (require, exports) {
+    "use strict";
+    var App = (function () {
+        function App() {
+            this.message = 'Hello World!';
+        }
+        return App;
+    }());
+    exports.App = App;
+});
+
+define('environment',["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.default = {
+        debug: true,
+        testing: true
+    };
+});
+
+define('main',["require", "exports", './environment'], function (require, exports, environment_1) {
+    "use strict";
+    function configure(aurelia) {
+        aurelia.use
+            .standardConfiguration()
+            .feature('resources');
+        if (environment_1.default.debug) {
+            aurelia.use.developmentLogging();
+        }
+        if (environment_1.default.testing) {
+            aurelia.use.plugin('aurelia-testing');
+        }
+        aurelia.start().then(function () { return aurelia.setRoot(); });
+    }
+    exports.configure = configure;
+});
+
+define('resources/index',["require", "exports"], function (require, exports) {
+    "use strict";
+    function configure(config) {
+    }
+    exports.configure = configure;
+});
+
+define('text!app.html', ['module'], function(module) { module.exports = "<template>\n  <require from=\"app.css\"></require>\n  <h1>${message}</h1>\n</template>\n"; });
+define('text!app.css', ['module'], function(module) { module.exports = "body {\n  background-color: red; }\n"; });
+//# sourceMappingURL=app-bundle.js.map
