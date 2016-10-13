6c4e3d6e2509a415100b98991503f9d1e6e0f09f
Meirion Hughes <crakinshot@yahoo.com>
2016-10-13 23:26:24 +0100

    stash dock panel testing

--- source/elements/index.ts
+++ source/elements/index.ts
@@ -1,5 +1,5 @@
 import {FrameworkConfiguration} from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration) {
-  config.globalResources(['./window', './tab-panel']);
+  config.globalResources(['./window', './tab-panel', './dock-panel']);
 }
