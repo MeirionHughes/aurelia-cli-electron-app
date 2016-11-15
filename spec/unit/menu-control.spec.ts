30a4dca56792a8462fe46b19c835fd146e86e7f9
javiercbk <javierlecuona@gmail.com>
2016-11-15 00:25:17 -0300

    updated dependencies. Created sample unit test

---
+++ spec/unit/menu-control.spec.ts
@@ -0,0 +1,42 @@
+import {StageComponent} from 'aurelia-testing';
+import {bootstrap} from 'aurelia-bootstrapper';
+import {App} from '../../src/app';
+
+describe('MenuControl', () => {
+  let component;
+  let menu;
+
+  beforeEach(() => {
+    menu = [
+      {
+        title: "Edit",
+        items: [
+          {
+            title: "Cut"
+          },
+          {
+            title: "Copy"
+          },
+          {
+            title: "Paste"
+          }
+        ]
+      }
+    ];
+    component = StageComponent
+      .withResources('elements/menu-control')
+      .inView('<menu-control items.bind="menu"></menu-control>')
+      .boundTo({ menu: menu });
+  });
+
+  it('should bind the entire menu', done => {
+    component.create(bootstrap).then(() => {
+      expect(component.viewModel.items).toBe(menu);
+      done();
+    });
+  });
+
+  afterEach(() => {
+    component.dispose();
+  });
+});
