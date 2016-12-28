875330c8b204bedd71a5b4fdf876601e4b8e4752
Angel D. Munoz <scythes.lol@gmail.com>
2016-12-27 23:50:09 -0700

    changed listeners to aurelia delegates

--- source/elements/window.ts
+++ source/elements/window.ts
@@ -1,42 +1,28 @@
 import { remote } from 'electron';
 
 export class Window {
-  title:string = "App";
-  
+  title: string = "App";
+
   constructor() {
-  }
-
-  attached() {
-    document.getElementById("min-btn").addEventListener("click", (e) => {
-      var window: Electron.BrowserWindow = remote.getCurrentWindow();
-      window.minimize();
-    });
-
-    document.getElementById("max-btn").addEventListener("click", (e) => {
-      var window: Electron.BrowserWindow = remote.getCurrentWindow();
-
-      if (window.isMaximized())
-        window.unmaximize();
-      else
-        window.maximize();
-    });
-
-    document.getElementById("close-btn").addEventListener("click", (e) => {
-      this.tryCloseWindow();
-    });
-
-    document.getElementById("help-btn").addEventListener("click", (e) => {
-      this.showHelp();
-    });
   }
 
   tryCloseWindow() {
     this.closeWindow();
   }
 
+  minimizeWindow() {
+    var window: Electron.BrowserWindow = remote.getCurrentWindow();
+    window.minimize();
+  }
+
+  maximizeWindow() {
+    var window: Electron.BrowserWindow = remote.getCurrentWindow();
+    if (window.isMaximized()) { window.unmaximize(); }
+    else { window.maximize(); }
+  }
+
   closeWindow() {
     var window: Electron.BrowserWindow = remote.getCurrentWindow();
-
     window.close();
   }
 
